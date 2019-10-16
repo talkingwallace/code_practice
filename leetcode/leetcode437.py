c322ffa356c06354cccc896cb3cbb02306e3eca2
weijing <talkingwallace@sohu.com>
2019-10-16 23:40:55 +0800

    437

--- leetcode/leetcode437.py
+++ leetcode/leetcode437.py
@@ -4,6 +4,46 @@
 from leetcode.public_class import TreeNode
 
 class Solution:
+    val_map = {}
+    sum = 114514
+    count = 0
+    node_idx = 0
+
+    def preorder(self, node):
+
+        if not node:
+            return
+        self.node_idx += 1
+
+        for k in self.val_map:
+            new_val = self.val_map[k] + node.val
+            self.val_map[k] = new_val
+            if new_val == self.sum:
+                self.count += 1
+
+        cur_idx = self.node_idx
+        self.val_map[cur_idx] = node.val
+        if node.val == self.sum:
+            self.count += 1
+
+        self.preorder(node.left)
+        self.preorder(node.right)
+
+        self.val_map.pop(cur_idx)
+        for k in self.val_map:
+            self.val_map[k] = self.val_map[k] - node.val
 
     def pathSum(self, root: TreeNode, sum):
-        pass
+
+        if not root:
+            return 0
+
+        # initialize
+        self.val_map = {}
+        self.sum = sum
+        self.count = 0
+        self.node_idx = 0
+
+        self.preorder(root)
+
+        return self.count
